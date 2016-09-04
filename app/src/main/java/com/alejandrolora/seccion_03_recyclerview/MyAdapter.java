6de0b947155c30c847747a0010e525f6d725dbf2
Alejandro <alejandrofpo@gmail.com>
2016-09-04 02:54:17 +0100

    first commit

---
+++ app/src/main/java/com/alejandrolora/seccion_03_recyclerview/MyAdapter.java
@@ -0,0 +1,79 @@
+package com.alejandrolora.seccion_03_recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Alejandro on 3/9/16.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> names;
+    private int layout;
+    private OnItemClickListener itemClickListener;
+
+
+    public MyAdapter(List<String> names, int layout, OnItemClickListener listener) {
+        this.names = names;
+        this.layout = layout;
+        this.itemClickListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflamos el layout y se lo pasamos al constructor del ViewHolder, donde manejaremos
+        // toda la lógica como extraer los datos, referencias...
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // Llamamos al método Bind del ViewHolder pasándole objeto y listener
+        holder.bind(names.get(position), itemClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Elementos UI a rellenar
+        public TextView textViewName;
+
+        public ViewHolder(View itemView) {
+            // Recibe la View completa. La pasa al constructor padre y enlazamos referencias UI
+            // con nuestras propiedades ViewHolder declaradas justo arriba.
+            super(itemView);
+            this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+        }
+
+        public void bind(final String name, final OnItemClickListener listener) {
+            // Procesamos los datos a renderizar
+            this.textViewName.setText(name);
+            // Definimos que por cada elemento de nuestro recycler view, tenemos un click listener
+            // que se comporta de la siguiente manera...
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // ... pasamos nuestro objeto modelo (este caso String) y posición
+                    listener.onItemClick(name, getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    // Declaramos nuestra interfaz con el/los método/s a implementar
+    public interface OnItemClickListener {
+        void onItemClick(String name, int position);
+    }
+
+
+}
